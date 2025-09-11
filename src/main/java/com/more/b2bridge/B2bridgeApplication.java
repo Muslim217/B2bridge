6cf1929edd9ad25dfc909ee7090bd8aac0e3178d
Muslim217 <abumuslimsafaraliev3815@gmail.com>
2025-09-11 09:39:36 +0300

    Создал структуру модулей
    (cherry picked from commit 1505fff1dff657dbfc4ab8c5c9352d92a41f7730)

---
+++ src/main/java/com/more/b2bridge/B2bridgeApplication.java
@@ -0,0 +1,13 @@
+package com.more.b2bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2bridgeApplication.class, args);
+	}
+
+}
