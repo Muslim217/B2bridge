4b6925d37f30130094514bfc2e3e33df43824eda
Muslim217 <abumuslimsafaraliev3815@gmail.com>
2025-09-22 16:41:22 +0300

    Коммит с откатом до одномодульного проекта

---
+++ src/test/java/com/more/b2bridge/B2bridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.more.b2bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
