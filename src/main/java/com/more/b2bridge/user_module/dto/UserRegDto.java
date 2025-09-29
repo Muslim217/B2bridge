2ccf7cb87f0f270a816524af7b7c9aafb6c5552f
ELEZAR-77 <omarabdullaev@gmail.com>
2025-09-29 05:34:51 +0300

    Reapply "Написал register post запрос"
    This reverts commit 7461479215482fc07745a4cd172f40783cbda79a.

---
+++ src/main/java/com/more/b2bridge/user_module/dto/UserRegDto.java
@@ -0,0 +1,13 @@
+package com.more.b2bridge.user_module.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegDto {
+    private String username;
+    private String email;
+    private String password;
+}
