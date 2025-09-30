ac312e5bedafa5feed0431a392efe84688255d2f
amirboomerr <mr.troll936dfg@gmail.com>
2025-09-30 12:15:42 +0300

    feat: реализация контроллера, сервиса, класса ProductImage

--- src/main/java/com/more/b2bridge/product_module/model/ProductImage.java
+++ src/main/java/com/more/b2bridge/product_module/model/ProductImage.java
@@ -1,4 +1,19 @@
 package com.more.b2bridge.product_module.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bitecode;
+    private boolean isPrimary;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
--- src/main/java/com/more/b2bridge/product_module/service/ProductService.java
+++ src/main/java/com/more/b2bridge/product_module/service/ProductService.java
@@ -1,4 +1,30 @@
 package com.more.b2bridge.product_module.service;
 
+import com.more.b2bridge.product_module.model.Product;
+import com.more.b2bridge.product_module.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ProductService {
+    @Autowired
+    ProductRepo repo;
+
+    public Optional<Product> getProduct(Long productId) {
+       return repo.findById(productId);
+    }
+
+    public void createProduct(Product product) {
+        repo.save(product);
+    }
+
+    public void deleteProduct(Long productId) {
+        repo.deleteById(productId);
+    }
+
+    public void updateProduct(Product product) {
+        repo.save(product);
+    }
 }
